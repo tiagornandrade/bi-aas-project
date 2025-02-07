f62e4f352735f0468444a5fce7b04da367de2a36
Tiago Ribeiro Navarro de Andrade <42575083+tiagornandrade@users.noreply.github.com>
2025-02-07 14:41:24 -0300

    feat: adiciona pasta terraform

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  credentials = file("credential/credential.json")
+}
+
+module "airbyte" {
+  source                 = "./modules/airbyte"
+  project_id             = var.project_id
+  region                 = var.region
+  zone                   = var.zone
+  machine_type           = var.machine_type
+  service_account_email  = var.service_account_email
+}
