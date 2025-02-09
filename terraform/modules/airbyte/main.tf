849c438c61d205bda0912b509862f88ac01afc62
Tiago Ribeiro Navarro de Andrade <42575083+tiagornandrade@users.noreply.github.com>
2025-02-09 08:09:12 -0300

    feat: atualiza action terraform_plan

--- terraform/modules/airbyte/main.tf
+++ terraform/modules/airbyte/main.tf
@@ -1,22 +1,22 @@
-resource "google_compute_instance" "airbyte_vm" {
-  name         = "airbyte-vm"
-  machine_type = var.machine_type
-  zone         = var.zone
+# resource "google_compute_instance" "airbyte_vm" {
+#   name         = "airbyte-vm"
+#   machine_type = var.machine_type
+#   zone         = var.zone
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      size  = 50
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#       size  = 50
+#     }
+#   }
 
-  network_interface {
-    network = "default"
-    access_config {}
-  }
+#   network_interface {
+#     network = "default"
+#     access_config {}
+#   }
 
-  service_account {
-    email  = var.service_account_email
-    scopes = ["cloud-platform"]
-  }
-}
+#   service_account {
+#     email  = var.service_account_email
+#     scopes = ["cloud-platform"]
+#   }
+# }
