84fada0b17cc172944dae1e1ed394b71969237f3
Tiago Ribeiro Navarro de Andrade <42575083+tiagornandrade@users.noreply.github.com>
2025-02-09 08:09:57 -0300

    feat: atualiza action terraform_plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,14 +3,14 @@ provider "google" {
   region      = var.region
 }
 
-module "airbyte" {
-  source                = "./modules/airbyte"
-  project_id            = var.project_id
-  region                = var.region
-  zone                  = var.zone
-  machine_type          = var.machine_type
-  service_account_email = var.service_account_email
-}
+# module "airbyte" {
+#   source                = "./modules/airbyte"
+#   project_id            = var.project_id
+#   region                = var.region
+#   zone                  = var.zone
+#   machine_type          = var.machine_type
+#   service_account_email = var.service_account_email
+# }
 
 terraform {
   backend "gcs" {
