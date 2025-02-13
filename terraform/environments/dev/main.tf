28db88a86a9f5e64350fbf7eecf0150137754f56
Tiago Ribeiro Navarro de Andrade <42575083+tiagornandrade@users.noreply.github.com>
2025-02-13 19:22:43 -0300

    adiciona iac postgres

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -7,11 +7,11 @@
 #   service_account_email = var.service_account_email
 # }
 
-module "postgres" {
-  source                = "../../modules/postgres"
-  project_id            = var.project_id
-  region                = var.region
-  zone                  = var.zone
-  machine_type          = var.machine_type
-  service_account_email = var.service_account_email
-}
+# module "postgres" {
+#   source                = "../../modules/postgres"
+#   project_id            = var.project_id
+#   region                = var.region
+#   zone                  = var.zone
+#   machine_type          = var.machine_type
+#   service_account_email = var.service_account_email
+# }
