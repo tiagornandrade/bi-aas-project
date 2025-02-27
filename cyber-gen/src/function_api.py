793e2107a00a09d37daca27736b17e8ea4c6fe18
Tiago Ribeiro Navarro de Andrade <42575083+tiagornandrade@users.noreply.github.com>
2025-02-27 17:25:57 -0300

    fix: ajusta arquivo do airflow

--- cyber-gen/src/function_api.py
+++ cyber-gen/src/function_api.py
@@ -1,9 +1,15 @@
 import os
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 from datetime import datetime
 import uvicorn
+from datetime import timezone
 from src.utils.query_manager import execute_query
-from src.utils.firestore import create_status_record, update_status, db
+from src.utils.firestore import (
+    DatastoreClient,
+    create_status_record,
+    get_datastore_client,
+    update_status,
+)
 
 app = FastAPI()
 
@@ -15,7 +21,7 @@ def process_job(layer: str, table: str):
     doc_id = None
 
     try:
-        start_time = datetime.utcnow().isoformat()
+        start_time = datetime.now(timezone.utc).isoformat()
         end_time = start_time
 
         doc_id = create_status_record(table, "execute", start_time, end_time)
@@ -26,15 +32,15 @@ def process_job(layer: str, table: str):
         update_status(doc_id, "success")
         return {"status": "Execução concluída", "job_id": doc_id}
 
-    except FileNotFoundError:
+    except FileNotFoundError as e:
         if doc_id:
             update_status(doc_id, "failed")
-        raise HTTPException(status_code=404, detail="Query não encontrada")
+        raise HTTPException(status_code=404, detail="Query não encontrada") from e
 
     except Exception as e:
         if doc_id:
             update_status(doc_id, "failed")
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e)) from e
 
 
 @app.get("/")
@@ -45,25 +51,44 @@ def read_root():
 @app.post("/{layer}/{table}/execute")
 def execute_table(layer: str, table: str):
     """Executa a query correspondente à camada e registra o status."""
-    if layer not in ["bronze", "silver", "gold"]:
+    if layer in {"bronze", "silver", "gold"}:
+        return process_job(layer, table)
+    else:
         raise HTTPException(status_code=400, detail="Camada inválida")
 
-    return process_job(layer, table)
 
-
-@app.get("/status/{job_id}")
-def get_status(job_id: str):
-    """Retorna o status de um job pelo ID."""
+@app.post("/status/")
+def create_status(
+    table: str,
+    job_type: str,
+    start_time: str,
+    end_time: str,
+    datastore_client: DatastoreClient = Depends(get_datastore_client),
+):
+    """Cria um status no Datastore."""
     try:
-        task_key = db.key("etl_jobs", job_id)
-        entity = db.get(task_key)
-
-        if entity:
-            return entity
-        raise HTTPException(status_code=404, detail="Job não encontrado")
-
+        job_id = datastore_client.create_status_record(
+            table, job_type, start_time, end_time
+        )
+        return {"message": "Job criado com sucesso", "job_id": job_id}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e)) from e
+
+
+@app.patch("/status/{job_id}")
+def update_status(
+    job_id: str,
+    status: str,
+    datastore_client: DatastoreClient = Depends(get_datastore_client),
+):
+    """Atualiza o status de um job no Datastore."""
+    try:
+        datastore_client.update_status(job_id, status)
+        return {"message": "Status atualizado com sucesso"}
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e)) from e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) from e
 
 
 if __name__ == "__main__":
--- cyber-gen/src/utils/firestore.py
+++ cyber-gen/src/utils/firestore.py
@@ -1,41 +1,50 @@
 from google.cloud import datastore
 from datetime import datetime, timezone
 
-db = datastore.Client()
-
 KIND = "etl_jobs"
 
 
-def create_status_record(table: str, job_type: str, start_time: str, end_time: str):
-    """Cria um registro de status no Google Datastore com status 'queued'."""
-    timestamp = datetime.now(timezone.utc).strftime("%Y%m%dT%H%M%S")
-    doc_id = f"{table}_{job_type}_{timestamp}"
+class DatastoreClient:
+    """Gerencia a conexão com o Google Datastore."""
 
-    task_key = db.key(KIND, doc_id)
-    entity = datastore.Entity(key=task_key)
+    def __init__(self):
+        self.client = datastore.Client()
 
-    entity.update(
-        {
-            "table": table,
-            "type": job_type,
-            "start_time": start_time,
-            "end_time": end_time,
-            "status": "queued",
-            "created_at": datetime.now(timezone.utc).isoformat(),
-            "updated_at": datetime.now(timezone.utc).isoformat(),
-        }
-    )
+    def create_status_record(
+        self, table: str, job_type: str, start_time: str, end_time: str
+    ):
+        """Cria um registro de status no Google Datastore com status 'queued'."""
+        timestamp = datetime.now(timezone.utc).strftime("%Y%m%dT%H%M%S")
+        doc_id = f"{table}_{job_type}_{timestamp}"
 
-    db.put(entity)
-    return doc_id
+        task_key = self.client.key(KIND, doc_id)
+        entity = datastore.Entity(key=task_key)
+
+        entity.update(
+            {
+                "table": table,
+                "type": job_type,
+                "start_time": start_time,
+                "end_time": end_time,
+                "status": "queued",
+                "created_at": datetime.now(timezone.utc).isoformat(),
+                "updated_at": datetime.now(timezone.utc).isoformat(),
+            }
+        )
+
+        self.client.put(entity)
+        return doc_id
+
+    def update_status(self, doc_id: str, status: str):
+        """Atualiza o status de um job no Google Datastore."""
+        if entity := self.client.get(self.client.key(KIND, doc_id)):
+            entity["status"] = status
+            entity["updated_at"] = datetime.now(timezone.utc).isoformat()
+            self.client.put(entity)
+            return
+        raise ValueError(f"Registro com ID {doc_id} não encontrado.")
 
 
-def update_status(doc_id: str, status: str):
-    """Atualiza o status de um job no Google Datastore."""
-    if entity := db.get(db.key(KIND, doc_id)):
-        entity["status"] = status
-        entity["updated_at"] = datetime.now(timezone.utc).isoformat()
-        db.put(entity)
-        return
-
-    raise ValueError(f"Registro com ID {doc_id} não encontrado.")
+def get_datastore_client():
+    """Fornece uma instância do DatastoreClient."""
+    return DatastoreClient()
